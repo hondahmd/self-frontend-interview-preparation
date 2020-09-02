c37da2b7c77012937cba659565dbccb0807e7600
hondahmd <1535307545@qq.com>
2020-09-02 20:22:07 +0800

    leetcode: easy dynamic-programming optimization for 1

--- leetcode/easy/dynamic-programming/1.js
+++ leetcode/easy/dynamic-programming/1.js
@@ -19,4 +19,19 @@ var climbStairs = function(n) {
     return result;
 };
 
+var climbStairs = function(n) {
+    const stepArr = Array(n + 1);
+    stepArr[1] = 1;
+    stepArr[2] = 2;
+    
+    const oneStep = (current) => {
+        if (stepArr[current]) return stepArr[current];
+        stepArr[current] = oneStep(current - 1) + oneStep(current - 2);
+        return stepArr[current];
+    }
+    
+    oneStep(n);
+    return stepArr[n];
+};
+
 console.log(climbStairs(44));
