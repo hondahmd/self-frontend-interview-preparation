4472f92e3d9592299b95a3832371b4fb6f8e540f
hondahmd <1535307545@qq.com>
2021-02-22 23:32:54 +0800

    understanding: complete inheritance examples

--- understanding/object.js
+++ understanding/object.js
@@ -38,8 +38,71 @@ function Person3 (name) {
     }
 }
 
-const test3_1 = new Person3('Honda');
-const test3_2 = new Person3('Juele');
+// const test3_1 = new Person3('Honda');
+// const test3_2 = new Person3('Juele');
 // test3_1.showName();
 // test3_2.showName();
 // console.log(test3_1.showName === test3_2.showName);
+
+/** 继承 */
+// 借用构造函数
+function Super1 () {
+  this.colors = [];
+}
+
+function Sub1 () {
+  Super1.call(this);
+}
+
+// const test4_1 = new Sub1();
+// const test4_2 = new Sub1();
+// console.log(test4_1 === test4_2);
+
+// 组合继承
+function Super2 (name) {
+  this.name = name;
+}
+
+Super2.prototype.showName = function () {
+  console.log(this.name);
+}
+
+function Sub2 (name) {
+  Super2.call(this, name);
+}
+
+Sub2.prototype = new Super2();
+Sub2.prototype.constructor = Sub2;
+
+const test5_1 = new Sub2('Honda');
+const test5_2 = new Sub2('Wow');
+console.log(test5_1 instanceof Super2);
+console.log(test5_1.showName === test5_2.showName);
+
+// 寄生组合式继承
+function Super3 (name) {
+  this.name = name;
+  this.colors = [];
+}
+
+function Sub3 (name) {
+  Super3.call(this, name);
+}
+
+// Sub的原型需要是super的原型的副本，用一个持有super原型的新的构造函数来构建sub的原型
+// 这里的prototypeFn其实就是替换了组合继承中的Super构造函数
+function inheritPrototype (subType, superType) {
+  const prototypeFn = function () {};
+  prototypeFn.prototype = superType.prototype;
+  const prototype = new prototypeFn();
+  prototype.constructor = subType;
+  subType.prototype = prototype;
+}
+
+inheritPrototype(Sub3, Super3);
+
+const test6_1 = new Sub3('Honda');
+const test6_2 = new Sub3('How');
+test6_1.colors.push('red');
+console.log(test6_1, test6_2);
+console.log(test6_1 instanceof Super3);
