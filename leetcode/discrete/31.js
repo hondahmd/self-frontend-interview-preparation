5ffa5b290ac16cc4f557360bbf93867d90a395d2
hongmingda <hongmingda@kuaishou.com>
2021-09-05 22:52:26 +0800

    leetcode discrete 31 finish

--- leetcode/discrete/31.js
+++ leetcode/discrete/31.js
@@ -2,28 +2,35 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var nextPermutation = function(nums) {
-    const length = nums.length;
-    let flag = -1;
-    for (let i = length - 2; i >= 0; i--) {
-        if (nums[i] < nums[i+1]) {
-            flag = i;
-            break;
+var nextPermutation = function (nums) {
+  const bubbleSort = (start, end) => {
+    for (let i = start; i <= end; i++) {
+      for (let j = i; j <= end; j++) {
+        if (nums[j] < nums[i]) {
+          const temp = nums[j];
+          nums[j] = nums[i];
+          nums[i] = temp;
         }
+      }
     }
+  }
 
-    for (let i = length - 1; i >=0; i--) {
-        if (nums[i] > nums[flag]) {
-            const temp = nums[i];
-            nums[i] = nums[flag];
-            nums[flag] = temp;
+  const length = nums.length;
+  let flag = -1;
+  for (let i = length - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      flag = i;
+      for (let j = length - 1; j >= 0; j--) {
+        if (nums[j] > nums[flag]) {
+          const temp = nums[j];
+          nums[j] = nums[flag];
+          nums[flag] = temp;
+          break;
         }
+      }
+      break;
     }
+  }
 
-    const swapL = length - flag - 1;
-    for (let i = 0; i < Math.floor(swapL / 2); i++) {
-        const temp = nums[flag + 1 + i];
-        nums[flag + 1 + i] = nums[length - 1 - i];
-        nums[length - 1 - i] = temp;
-    }
+  bubbleSort(flag + 1, length - 1);
 };
