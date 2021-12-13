cc9222631a9f849f33553dc41ce8c08ede4a8b8b
hongmingda <hongmingda@kuaishou.com>
2021-12-13 23:56:11 +0800

    leetcode: discrete 40 finish

--- leetcode/discrete/40.js
+++ leetcode/discrete/40.js
@@ -1,31 +1,46 @@
 var combinationSum = function (candidates, target) {
-  const sum = (arr) => arr.reduce((acc, cur) => acc += cur, 0);
-  if (sum(candidates) === target) {
-    return [candidates];
+  const allSame = () => {
+    const first = candidates[0];
+    for (let i = 1; i < candidates.length; i++) {
+      if (first !== candidates[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  if (allSame() && ((target % candidates[0]) === 0) && ((target / candidates[0]) <= candidates.length)) {
+    return [Array(target / candidates[0]).fill(candidates[0])];
   }
   const result = {};
 
   candidates.sort((a, b) => a - b);
-  const oneLayer = (current, index) => {
-    const key = current.join('');
-    if (sum(current) === target) {
-      if (!result[key]) {
-        result[key] = current;
+  const sum = (arr) => arr.reduce((acc, cur) => acc += cur, 0);
+  const current = [];
+  const oneLayer = (index) => {
+    if (sum(current) + candidates[index] === target) {
+      if (!result[current.join('') + candidates[index]]) {
+        result[current.join('') + candidates[index]] = [...current, candidates[index]];
       }
       return;
     }
-    if (sum(current) > target) {
+    if (sum(current) + candidates[index] > target) {
       return;
     }
+    current.push(candidates[index]);
     for (let i = index; i < candidates.length; i++) {
-      oneLayer([...current, candidates[i]], i + 1);
+      oneLayer(i + 1);
     }
+    current.pop();
   };
-  oneLayer([], 0);
+  candidates.forEach((item, index) => {
+    oneLayer(index);
+  })
 
   return Object.values(result);
 };
 
 const candidates = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 const target = 30;
+// const candidates = [10, 1, 2, 7, 6, 1, 5];
+// const target = 8;
 console.log(combinationSum(candidates, target));
