a6b078d6512775a3f09f7844c7bd3edc879a996d
hongmingda <hongmingda@kuaishou.com>
2021-12-14 23:30:36 +0800

    leetcode: discrete 630 update

--- leetcode/discrete/630.js
+++ leetcode/discrete/630.js
@@ -1,21 +1,37 @@
-var scheduleCourse = function(courses) {
-  let result = 0;
-  const oneLayer = (left, current) => {
-    if ((courses.length - left.length) > result) {
-      result = courses.length - left.length;
-    }
-    if (left.length === 0) {
-      return;
-    }
-    left.forEach(course => {
-      if ((current + courses[course][0]) <= courses[course][1]) {
-        oneLayer(left.filter(item => item !== course), current + courses[course][0]);
+var scheduleCourse = function (courses) {
+  const findUrgeCourse = (courses) => {
+    let resultIndex = 0;
+    let resultOffset = courses[0][1] - courses[0][0];
+    courses.forEach((course, index) => {
+      if ((course[1] - course[0]) < resultOffset) {
+        resultOffset = course[1] - course[0];
+        resultIndex = index;
       }
-    });
+    })
+    return resultIndex;
   };
-  oneLayer(courses.map((item, index) => index), 0);
+
+  const clone = (arr) => arr.map(course => [...course]);
+
+  let result = 0;
+  while (courses.length > 0) {
+    const temp = clone(courses);
+    let loopResult = 0;
+    let current = 0;
+    while (temp.length > 0) {
+      const next = findUrgeCourse(temp);
+      if (current + temp[next][0] <= temp[next][1]) {
+        current += temp[next][0];
+        loopResult++;
+      }
+      temp.splice(next, 1);
+    }
+    result = Math.max(result, loopResult);
+    courses.splice(findUrgeCourse(courses), 1);
+  }
+
   return result;
 };
 
-const courses = [[100, 200], [200, 1300], [1000, 1250], [2000, 3200]];
+const courses = [[7, 16], [2, 3], [3, 12], [3, 14], [10, 19], [10, 16], [6, 8], [6, 11], [3, 13], [6, 16]];
 console.log(scheduleCourse(courses));
