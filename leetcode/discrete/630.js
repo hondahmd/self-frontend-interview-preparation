b08bbfa17ce7ced17b6786f0d5849e5ba2ebde95
hongmingda <hongmingda@kuaishou.com>
2021-12-15 20:14:55 +0800

    leetcode: discrete 630 finish (copy)

--- leetcode/discrete/630.js
+++ leetcode/discrete/630.js
@@ -1,41 +1,23 @@
 var scheduleCourse = function (courses) {
-  const terms = [];
-  courses
-    .forEach((course, index) => {
-      const exist = terms.findIndex(term => term.end === course[1]);
-      if (exist !== -1) {
-        terms[exist].list.push(index);
-      } else {
-        terms.push({
-          end: course[1],
-          list: [index],
-        })
-      }
-    })
-  terms.sort((a, b) => a.end - b.end);
-  terms.forEach(term => {
-    courses.forEach((course, index) => {
-      if (course[0] <= term.end && course[1] > term.end) {
-        term.list.push(index);
-      }
-    });
-    term.list = Array.from(new Set(term.list));
-    term.list.sort((a, b) => courses[a][0] - courses[b][0]);
-  });
+  courses.sort((a, b) => a[1] - b[1]);
+
+  const result = [];
 
   let current = 0;
-  const finished = [];
-  terms.forEach(term => {
-    term.list.forEach(index => {
-      if (!finished.includes(index) && current + courses[index][0] <= term.end) {
-        current += courses[index][0];
-        finished.push(index);
-      }
-    })
+  courses.forEach(course => {
+    if (current + course[0] <= course[1]) {
+      current += course[0];
+      result.push(course[0]);
+    } else if (result.length > 0 && result[result.length - 1] > course[0]) {
+      current -= result.pop();
+      current += course[0];
+      result.push(course[0]);
+    }
+    result.sort((a, b) => a - b)
   });
 
-  return finished.length;
+  return result.length;
 };
 
-const courses = [[5,15],[3,19],[6,7],[2,10],[5,16],[8,14],[10,11],[2,19]];
+const courses = [[7,17],[3,12],[10,20],[9,10],[5,20],[10,19],[4,18]];
 console.log(scheduleCourse(courses));
