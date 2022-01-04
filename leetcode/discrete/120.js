3a317a49b59d6ea142dee3e0b91e4f04b2449742
hongmingda <hongmingda@kuaishou.com>
2022-01-04 14:11:13 +0800

    leetcode 120 finish

--- leetcode/discrete/120.js
+++ leetcode/discrete/120.js
@@ -1,15 +1,14 @@
 const minimumTotal = (triangle) => {
-  const dp = Array(triangle.length).fill(Infinity);
-  dp[0] = triangle[0][0];
+  const dp = [...triangle[triangle.length - 1]];
 
-  triangle.slice(0, -1).forEach((layer, step) => {
+  triangle.slice(0, -1).reverse().forEach((layer) => {
     layer.forEach((node, index) => {
-      dp[step + 1] = Math.min(dp[step + 1], dp[step] + triangle[step + 1][index], dp[step] + triangle[step + 1][index + 1]);
+      dp[index] = Math.min(dp[index], dp[index + 1]) + node;
     });
   });
 
-  return dp[dp.length - 1];
+  return dp[0];
 }
 
-const triangle = [[-1],[2,3],[1,-1,-3]];
-minimumTotal(triangle)
+const triangle = [[2],[3,4],[6,5,7],[4,1,8,3]];
+console.log(minimumTotal(triangle));
